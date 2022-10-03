3f5dca066e4d34de9dd2e609f2196a7f008d8496
wys899195 <wys899195@gmail.com>
2022-10-03 14:33:00 +0800

    Initial Commit

---
+++ iOSappInterface/iOSappInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSappInterfaceApp.swift
+//  iOSappInterface
+//
+//  Created by User20 on 2022/10/3.
+//
+
+import SwiftUI
+
+@main
+struct iOSappInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
